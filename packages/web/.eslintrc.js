7f0a35e10970000fc3a745ce70abfc88cfa9f8b3
frontendmonster <frontendmonster@gmail.com>
2020-10-03 15:26:39 +0330

    chore(web): init web package

---
+++ packages/web/.eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  root: true,
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+  extends: '@fullstacksjs',
+};
---
+++ packages/web/src/pages/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+  return <div>Fullstacks</div>;
+};
+
+export default Home;
